685e6b95f0df9de20a95604c934d715ba6d7d048
Chance Rebish <chance.rebish@tufts.edu>
2025-06-26 10:26:02 +0800

    removed some text clutter

--- lib/parsers/gemini.ts
+++ lib/parsers/gemini.ts
@@ -9,6 +9,11 @@ export async function parseGemini(html: string): Promise<Conversation> {
   const dom = new JSDOM(html);
   const document = dom.window.document;
 
+  //Remove all misc elements
+  const miscElems = document.querySelectorAll('[class*="avatar-gutter"], [class*="response-container-header"], [class*="response-container-footer"]');
+  miscElems.forEach(element => element.remove());
+
+  //Grab the chat history container
   const chatHistory = document.querySelector('[data-test-id="chat-history-container"]')?.innerHTML ?? '';
 
   return {
