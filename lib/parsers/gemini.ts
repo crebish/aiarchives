2b3e484cf06d347b686025e524417be4b981124c
Chance Rebish <chance.rebish@tufts.edu>
2025-06-28 19:27:52 +0800

    Testing different scraping method

--- lib/parsers/gemini.ts
+++ lib/parsers/gemini.ts
@@ -9,17 +9,35 @@ export async function parseGemini(html: string): Promise<Conversation> {
   const dom = new JSDOM(html);
   const document = dom.window.document;
 
-  //Remove all misc elements
-  const miscElems = document.querySelectorAll('[class*="avatar-gutter"], [class*="response-container-header"], [class*="response-container-footer"]');
-  miscElems.forEach(element => element.remove());
+  const styleTags = document.head.querySelectorAll('style');
+  let combinedStyles = '';
+  
+  styleTags.forEach(styleTag => {
+    combinedStyles += styleTag.outerHTML + '\n';
+  });
 
-  //Grab the chat history container
-  const chatHistory = document.querySelector('[data-test-id="chat-history-container"]')?.innerHTML ?? '';
+  const style = document.createElement("style");
+  style.textContent = combinedStyles;
+
+  // //Remove all misc elements
+  // const miscElems = document.querySelectorAll('[class*="avatar-gutter"], [class*="response-container-header"], [class*="response-container-footer"]');
+  // miscElems.forEach(element => element.remove());
+
+  // //Grab the chat history container
+  // const chatHistory = document.querySelector('[data-test-id="chat-history-container"]')?.innerHTML ?? "";
+
+  const chatHistory = document.querySelector('[data-test-id="chat-history-container"]')
+
+  if (chatHistory !== null) {
+    chatHistory.prepend(style);
+  }
+
+  const chat = chatHistory?.innerHTML ?? "";
 
   return {
     model: 'Gemini',
-    content: chatHistory,
+    content: chat,
     scrapedAt: new Date().toISOString(),
-    sourceHtmlBytes: chatHistory.length,
+    sourceHtmlBytes: chat.length,
   };
 }
