918d85581ec151165e7deb72c47f5e62c7de8143
Chance Rebish <chance.rebish@tufts.edu>
2025-06-30 09:36:53 +0800

    Removed clutter from scraping

--- lib/parsers/gemini.ts
+++ lib/parsers/gemini.ts
@@ -9,27 +9,26 @@ export async function parseGemini(html: string): Promise<Conversation> {
   const dom = new JSDOM(html);
   const document = dom.window.document;
 
+  // Grab the document styling
   const styleTags = document.head.querySelectorAll('style');
   let combinedStyles = '';
   
   styleTags.forEach(styleTag => {
     styleTag.textContent = "html{overflow:scroll}" + styleTag.textContent;
-  
     combinedStyles += styleTag.outerHTML + '\n';
   });
 
   const style = document.createElement("style");
   style.textContent = combinedStyles;
 
-  // //Remove all misc elements
-  // const miscElems = document.querySelectorAll('[class*="avatar-gutter"], [class*="response-container-header"], [class*="response-container-footer"]');
-  // miscElems.forEach(element => element.remove());
+  // Remove all misc elements
+  const miscElems = document.querySelectorAll('sources-list, mat-icon, [class*="actions-container-v2"]');
+  miscElems.forEach(element => element.remove());
 
-  // //Grab the chat history container
-  // const chatHistory = document.querySelector('[data-test-id="chat-history-container"]')?.innerHTML ?? "";
-
+  // Grab the chat history container
   const chatHistory = document.querySelector('[data-test-id="chat-history-container"]')
 
+  // Add the styling to the container
   if (chatHistory !== null) {
     chatHistory.prepend(style);
   }
